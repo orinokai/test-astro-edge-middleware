8af6a70d92a3fe14d919a69fcba69c35cd3c8ffd
Rob Stanford <me@robstanford.com>
2024-10-02 13:40:33 -0700

    middleware log

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,7 @@
 import { defineMiddleware } from "astro:middleware";
 
 // `context` and `next` are automatically typed
-export const onRequest = defineMiddleware((_, next) => next());
+export const onRequest = defineMiddleware((_, next) => {
+  console.log("Middleware Request received");
+  next();
+});
